28da263c68926a3576252cf3edaf254cb4db1791
Salih Emin <salihemin@about.me>
2014-02-14 13:04:15 +0200

    Initial Assets translate

--- el_gr/Assets.php
+++ el_gr/Assets.php
@@ -9,28 +9,28 @@
  *************************************************************************************/
 $languageStrings = array(
 	// Basic Strings
-	'Assets'        => 'Assets',
-	'SINGLE_Assets' => 'Asset',
-	'LBL_ADD_RECORD' => 'Add Asset',
-	'LBL_RECORDS_LIST' => 'Assets List',
+	'Assets'        => 'Κεφάλαια',
+	'SINGLE_Assets' => 'Kεφάλαιο',
+	'LBL_ADD_RECORD' => 'Προσθήκη Κεφαλαίου',
+	'LBL_RECORDS_LIST' => 'Λίστα Κεφαλαίων',
 
 	// Blocks
-	'LBL_ASSET_INFORMATION' => 'Asset Details',
+	'LBL_ASSET_INFORMATION' => 'Λεπτομέρειες Κεφαλαίου',
 
 	//Field Labels
-    'Asset No' => 'Asset No',
-	'Serial Number' => 'Serial Number',
-	'Date Sold' => 'Date Sold',
-	'Date in Service' => 'Date in Service',
-	'Tag Number' => 'Tag Number',
-	'Invoice Name' => 'Invoice Name',
-	'Shipping Method' => 'Shipping Method',
-	'Shipping Tracking Number' => 'Shipping Tracking Number',
-	'Asset Name' => 'Asset Name',
-	'Customer Name' => 'Customer Name',
-	'Notes' => 'Notes',
+    'Asset No' => 'No Κεφαλαίου',
+	'Serial Number' => 'Αύξων αριθμός',
+	'Date Sold' => 'Ημερομηνία Πώλησης',
+	'Date in Service' => 'Ημερομηνία σε Υπηρεσία',
+	'Tag Number' => 'Αριθμός Tag',
+	'Invoice Name' => 'Όνομα Τιμολογίου',
+	'Shipping Method' => 'Τρόπος Αποστολής',
+	'Shipping Tracking Number' => 'Αριθμός Παρακολούθησης Αποστολής',
+	'Asset Name' => 'Όνομα Κεφαλαίου',
+	'Customer Name' => 'Όνομα Πελάτη',
+	'Notes' => 'Σημειώσεις',
 
 	/*picklist values*/
-	'In Service'=>'In Service',
-	'Out-of-service'=>'Out-of-service',
+	'In Service'=>'Σε Υπηρεσία',
+	'Out-of-service'=>'Εκτός Υπηρεσίας',
 );
