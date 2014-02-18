c48abbdd32009de921d9134c3925d401ad35bef8
Salih Emin <salihemin@about.me>
2014-02-18 12:13:13 +0200

    Fixed typo, chaneged some values
    Συμβάν --> Γεγονότα
    Ημερομηνία Γέν --> Γενέθλια

--- el_gr/Calendar.php
+++ el_gr/Calendar.php
@@ -10,20 +10,20 @@
 $languageStrings = array(
 	// Basic Strings
 	'SINGLE_Calendar' => 'Εργασία',
-	'SINGLE_Events' => 'Συμβάν',
+	'SINGLE_Events' => 'Γεγονός',
 	'LBL_ADD_TASK' => 'Προσθήκη Εργασίας',
-	'LBL_ADD_EVENT' => 'Προσθήκη Συμβάντος',
+	'LBL_ADD_EVENT' => 'Προσθήκη Γεγονότου',
 	'LBL_RECORDS_LIST' => 'Λίστα Ημερολογίου',
-	'LBL_EVENTS' => 'Συμβάντα',
+	'LBL_EVENTS' => 'Γεγονότα',
 	'LBL_TODOS' => 'Προς εκτέλεση',
 	'LBL_CALENDAR_SETTINGS' => 'Ρυθμίσεις Ημερολογίου',
 	'LBL_CALENDAR_SHARING' => 'Κοινή χρήση Ημερολογίου',
-	'LBL_DEFAULT_EVENT_DURATION' => 'Προεπιλογή Διάρκειας Συμβάντος',
+	'LBL_DEFAULT_EVENT_DURATION' => 'Προεπιλογή Διάρκειας Γεγονότος',
 	'LBL_CALL' => 'Τηλεφώνησε',
 	'LBL_OTHER_EVENTS' => 'Άλλα Συμβάντα',
 	'LBL_MINUTES' => 'Λεπτά',
 	'LBL_SELECT_USERS' => 'Επιλογή Χρηστών',
-	'LBL_EVENT_OR_TASK' => 'Συμβάν / Εργασία',
+	'LBL_EVENT_OR_TASK' => 'Γεγονός / Εργασία',
 
 	// Blocks
 	'LBL_TASK_INFORMATION' => 'Λεπτομέρειες Εργασίας',
@@ -39,12 +39,12 @@ $languageStrings = array(
 	//Side Bar Names
 	'LBL_ACTIVITY_TYPES' => 'Τύποι Δραστηριοτήτων',
 	'LBL_CONTACTS_SUPPORT_END_DATE' => 'Ημερομηνία Λήξης Υποστήριξης',
-	'LBL_CONTACTS_BIRTH_DAY' => 'Ημερομηνία Γέννησης',
+	'LBL_CONTACTS_BIRTH_DAY' => 'Γενέθλια',
 	'LBL_ADDED_CALENDARS' => 'Προστεθειμένα Ημερολόγια',
 
 
 	//Activity Type picklist values
-	'Call' => 'Τηλεφώνησε',
+	'Call' => 'Τηλέφωνο',
 	'Meeting' => 'Συνάτηση',
 	'Task' => 'Εργασία',
 
@@ -60,7 +60,7 @@ $languageStrings = array(
 
 	'LBL_CHANGE_OWNER' => 'Αλλαγή Ιδιοκτήτη',
 
-	'LBL_EVENT' => 'Συμβάν',
+	'LBL_EVENT' => 'Γεγονός',
 	'LBL_TASK' => 'Εργασία',
 	'LBL_TASKS' => 'Εργασίες',
 
@@ -89,12 +89,12 @@ $languageStrings = array(
 	
 	//Import and Export Labels
 	'LBL_IMPORT_RECORDS' => 'Εισαγωγή Εγγραφών',
-	'LBL_RESULT' => 'Result',
-	'LBL_FINISH' => 'Finish',
+	'LBL_RESULT' => 'Αποτέλεσμα',
+	'LBL_FINISH' => 'Τέλος',
 	'LBL_TOTAL_TASKS_IMPORTED' => 'Αρ. Επιτυχημένης Εισαγωγής Εργασιών ',
 	'LBL_TOTAL_TASKS_SKIPPED' => 'Αρ. Εργασιών που παραλείφθηκαν, καθώς έλειπαν μία ή περισσότερες από τα απαιτούμενα πεδία ',
 	'LBL_TOTAL_EVENTS_IMPORTED' => 'Αρ. Επιτυχημένης Εισαγωγής Συμβάντων ',
-	'LBL_TOTAL_EVENTS_SKIPPED' => 'Αρ. Συμβάντων που παραλείφθηκαν, καθώς έλειπαν μία ή περισσότερες από τα απαιτούμενα πεδία ',
+	'LBL_TOTAL_EVENTS_SKIPPED' => 'Αρ. Γεγονότων που παραλείφθηκαν, καθώς έλειπαν μία ή περισσότερες από τα απαιτούμενα πεδία ',
 	
 	'ICAL_FORMAT' => 'Τύπος iCal',
 	'LBL_LAST_IMPORT_UNDONE'=>'Τελευταία σας Εισαγωγή έχει αναιρεθεί',
@@ -103,11 +103,11 @@ $languageStrings = array(
 );
 
 $jsLanguageStrings = array(
-	'LBL_ADD_EVENT_TASK' => 'Προσθήκη Συμβάν/Εργασία',
+	'LBL_ADD_EVENT_TASK' => 'Προσθήκη Γεγονός/Εργασία',
 	'JS_TASK_IS_SUCCESSFULLY_ADDED_TO_YOUR_CALENDAR' => 'Η Εργασία έχει προστεθεί επιτυχώς στο Ημερολόγιό σας',
     'LBL_SYNC_BUTTON' => 'Συγχρονισμός Τώρα',
     'LBL_SYNCRONIZING' => 'Συγχρονισμός....',
-    'LBL_NOT_SYNCRONIZED' => 'Δεν έχετε συγχρονιστεί ακόμα',
+    'LBL_NOT_SYNCRONIZED' => 'Δεν έχετε συγχρονίσει ακόμα',
     'LBL_FIELD_MAPPING' => 'Χαρτογράφηση πεδίου',
     'LBL_CANT_SELECT_CONTACT_FROM_LEADS' => 'Δεν είναι δυνατή η επιλογή των σχετικών Επαφών ως Προοπτικές',
     'JS_FUTURE_EVENT_CANNOT_BE_HELD' => 'Δεν μπορεί να θεωρηθεί Ολοκληρωμένο στο μέλλον',
