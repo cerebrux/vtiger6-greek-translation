53775e2c5e9de0279a77c734ac24d958ac1a5d45
Salih Emin <salihemin@about.me>
2014-02-19 10:40:50 +0200

    Initial Vendors translate

--- el_gr/Vendors.php
+++ el_gr/Vendors.php
@@ -9,38 +9,38 @@
  *************************************************************************************/
 $languageStrings = array(
 	// Basic Strings
-	'Vendors' => 'Vendors',
-	'SINGLE_Vendors' => 'Vendor',
-	'LBL_ADD_RECORD' => 'Add Vendor',
-	'LBL_RECORDS_LIST' => 'Vendors List',
+	'Vendors' => 'Προμηθευτές',
+	'SINGLE_Vendors' => 'Προμηθευτής',
+	'LBL_ADD_RECORD' => 'Προσθήκη Προμηθευτή',
+	'LBL_RECORDS_LIST' => 'Λίστα Προμηθευτών',
 
 	// Blocks
-	'LBL_VENDOR_INFORMATION' => 'Vendor Details',
-	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Address Details',
+	'LBL_VENDOR_INFORMATION' => 'Πληροφορίες Προμηθευτή',
+	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Πληροφορίες Διεύθυνσης',
 	
 	//Field Labels
-	'Vendor Name' => 'Vendor Name',
-	'Vendor No' => 'Vendor Number',
-	'Website' => 'Website',
-	'GL Account' => 'GL Account',
-	'Phone' => 'Primary Phone',
-	'Email' => 'Primary Email',
+	'Vendor Name' => 'Όνομα Προμηθευτή',
+	'Vendor No' => 'Αρ Προμηθευτή',
+	'Website' => 'Ιστοσελίδα',
+	'GL Account' => 'Λογαριασμός Γεν Λογιστικής',
+	'Phone' => 'Τηλέφωνο',
+	'Email' => 'Email',
 	
 	//Added for existing Picklist entries
 
-	'300-Sales-Software'=>'300-Sales-Software',
-	'301-Sales-Hardware'=>'301-Sales-Hardware',
-	'302-Rental-Income'=>'302-Rental-Income',
-	'303-Interest-Income'=>'303-Interest-Income',
-	'304-Sales-Software-Support'=>'304-Sales-Software-Support',
-	'305-Sales Other'=>'305-Sales Other',
-	'306-Internet Sales'=>'306-Internet Sales',
-	'307-Service-Hardware Labor'=>'307-Service-Hardware Labor',
-	'308-Sales-Books'=>'308-Sales-Books',
+	'300-Sales-Software'=>'300-Πωλήσεις-Software',
+	'301-Sales-Hardware'=>'301-Πωλήσεις-Hardware',
+	'302-Rental-Income'=>'302-Έσοδα-Ενοίκια',
+	'303-Interest-Income'=>'303-Έσοδα-Τόκοι',
+	'304-Sales-Software-Support'=>'304-Πωλήσεις-Υποστήριξη-Software',
+	'305-Sales Other'=>'305-Πωλήσεις-Άλλο',
+	'306-Internet Sales'=>'306-Πωλήσεις-Internet',
+	'307-Service-Hardware Labor'=>'307-Service-Hardware-Εργασία',
+	'308-Sales-Books'=>'308-Πωλήσεις-Βιβλία',
 );
 
 $jsLanguageStrings = array(
-	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to delete?',
-	'LBL_DELETE_CONFIRMATION' => 'Deleting this Vendor will remove its related PurchaseOrders. Are you sure you want to delete this Vendor?',
-	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this vendor(s) will remove its related Purchase Orders. Are you sure you want to delete the selected Records?',
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Είστε σίγουροι ότι θέλετε να διαγράψετε;',
+	'LBL_DELETE_CONFIRMATION' => 'Η διαγραφή αυτού του Προμηθευτή θα αφαιρέσει σχετικές εντολές αγοράς της. Είστε σίγουροι ότι θέλετε να διαγράψετε αυτό το Προμηθευτή;',
+	'LBL_MASS_DELETE_CONFIRMATION' => 'Διαγραφή αυτού αυτού του Προμηθευτή (ων) θα αφαιρέσει σχετικές εντολές αγοράς της. Είστε σίγουροι ότι θέλετε να διαγράψετε τα επιλεγμένα αρχεία;',
 );
