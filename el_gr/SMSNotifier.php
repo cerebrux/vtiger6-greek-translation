4f6e9fdf270820cdab3c153abc1f5d10a14f6472
mstavrou <m_stauroy@hotmail.com>
2014-03-06 13:19:30 +0200

    Update SMSNotifier.php

--- el_gr/SMSNotifier.php
+++ el_gr/SMSNotifier.php
@@ -8,17 +8,17 @@
  * All Rights Reserved.
  ************************************************************************************/
 $languageStrings = array(
-	'LBL_SEND_SMS_TO_SELECTED_NUMBERS' => 'Αποστολή μηνύματος στους επιλεγμένους αριθμούς',
-	'LBL_STEP_1'                   => 'Βήμα Πρώτο'                      ,
-	'LBL_STEP_2'                   => 'Βήμα Δεύτερο'                      ,
+	'LBL_SEND_SMS_TO_SELECTED_NUMBERS' => 'SMS',
+	'LBL_STEP_1'                   => 'Βήμα Πρώτο',
+	'LBL_STEP_2'                   => 'Βήμα Δεύτερο',
 	'LBL_SELECT_THE_PHONE_NUMBER_FIELDS_TO_SEND' => 'Επιλέξτε τα πεδία αριθμών τηλεφώνου για την αποστολή',
-	'LBL_TYPE_THE_MESSAGE'         => 'Πληκτρολογήστε το μήνυμα'            ,
-	'LBL_WRITE_YOUR_MESSAGE_HERE'  => 'γράψτε το μήνυμα σας εδώ'     ,
-	'LBL_ADD_MORE_FIELDS'          => 'Προσθέστε περισσότερα πεδία'             ,
-	'LBL_SERVER_CONFIG'           => 'Ρύθμιση παραμέτρων διακομιστή'        ,
-	'LBL_CHECK_STATUS'             => 'Ελέγξτε την κατάσταση'                ,
-	'message'                      => 'Μήνυμα'                     ,
-	'LBL_SMSNOTIFIER_INFORMATION'  => 'Πληροφορίες Μηνύματος'             ,
-	'SINGLE_SMSNotifier'           => 'Ειδοποίηση Μηνύματος'                , // TODO: Review
+	'LBL_TYPE_THE_MESSAGE'         => 'Πληκτρολογήστε το μήνυμα',
+	'LBL_WRITE_YOUR_MESSAGE_HERE'  => 'γράψτε το μήνυμα σας εδώ',
+	'LBL_ADD_MORE_FIELDS'          => 'Προσθέστε περισσότερα πεδία',
+	'LBL_SERVER_CONFIG'           => 'Ρύθμιση παραμέτρων διακομιστή',
+	'LBL_CHECK_STATUS'             => 'Ελέγξτε την κατάσταση',
+	'message'                      => 'Μήνυμα',
+	'LBL_SMSNOTIFIER_INFORMATION'  => 'Πληροφορίες Μηνύματος',
+	'SINGLE_SMSNotifier'           => 'Ειδοποίηση Μηνύματος', // TODO: Review
 	'SMSNotifier' => 'Ειδοποίηση Μηνύματος',
 );
