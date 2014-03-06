0e4917021a6c0ce875a55aad3a6002329f705be6
Salih Emin <salihemin@about.me>
2014-03-06 13:38:44 +0200

    Έβαλα πίσω τα σχόλια

--- el_gr/SMSNotifier.php
+++ el_gr/SMSNotifier.php
@@ -11,13 +11,20 @@ $languageStrings = array(
 	'LBL_SEND_SMS_TO_SELECTED_NUMBERS' => 'SMS',
 	'LBL_STEP_1'                   => 'Βήμα Πρώτο',
 	'LBL_STEP_2'                   => 'Βήμα Δεύτερο',
+	
+	// Basic Strings
+
 	'LBL_SELECT_THE_PHONE_NUMBER_FIELDS_TO_SEND' => 'Επιλέξτε τα πεδία αριθμών τηλεφώνου για την αποστολή',
 	'LBL_TYPE_THE_MESSAGE'         => 'Πληκτρολογήστε το μήνυμα',
 	'LBL_WRITE_YOUR_MESSAGE_HERE'  => 'γράψτε το μήνυμα σας εδώ',
 	'LBL_ADD_MORE_FIELDS'          => 'Προσθέστε περισσότερα πεδία',
 	'LBL_SERVER_CONFIG'           => 'Ρύθμιση παραμέτρων διακομιστή',
+
+	//DetailView Actions
 	'LBL_CHECK_STATUS'             => 'Ελέγξτε την κατάσταση',
 	'message'                      => 'Μήνυμα',
+
+	//Blocks
 	'LBL_SMSNOTIFIER_INFORMATION'  => 'Πληροφορίες Μηνύματος',
 	'SINGLE_SMSNotifier'           => 'Ειδοποίηση Μηνύματος', // TODO: Review
 	'SMSNotifier' => 'Ειδοποίηση Μηνύματος',
