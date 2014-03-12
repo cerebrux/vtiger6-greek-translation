62f3f11e056c1b3333f4d2496eb47b03ab38d7b0
Kostas Paraslevopoulos <paraskeyopoulos@gmail.com>
2014-03-12 16:23:18 +0200

    #translated Leads.php
    @Lead was translated to Σύσταση , needs review

--- modules/Leads.php
+++ modules/Leads.php
@@ -9,67 +9,67 @@
  *************************************************************************************/
 $languageStrings = array(
 	// Basic Strings
-	'Leads' => 'Leads',
-	'SINGLE_Leads' => 'Lead',
-	'LBL_RECORDS_LIST' => 'Leads List',
-	'LBL_ADD_RECORD' => 'Add Lead',
+	'Leads' => 'Συστάσεις',
+	'SINGLE_Leads' => 'Σύσταση',
+	'LBL_RECORDS_LIST' => 'Λίστα συστάσεων',
+	'LBL_ADD_RECORD' => 'Προσθήκη σύστασης',
 
 	// Blocks
-	'LBL_LEAD_INFORMATION' => 'Lead Details',
+	'LBL_LEAD_INFORMATION' => 'Πληροφορίες σύστασης',
 
 	//Field Labels
-	'Lead No' => 'Lead Number',
-	'Company' => 'Company',
+	'Lead No' => 'Αριθμός σύστασης',
+	'Company' => 'Εταιρία',
 	'Designation' => 'Designation',
-	'Website' => 'Website',
-	'Industry' => 'Industry',
-	'Lead Status' => 'Lead Status',
-	'No Of Employees' => 'Number of Employees',
-	'Phone' => 'Primary Phone',
-	'Secondary Email' => 'Secondary Email',
-	'Email' => 'Primary Email',
+	'Website' => 'Ιστότοπος',
+	'Industry' => 'Βιομηχανιας',
+	'Lead Status' => 'Κατάσταση σύστασης',
+	'No Of Employees' => 'Αριθμός υπαλλήλων',
+	'Phone' => 'Τηλέφωνο',
+	'Secondary Email' => 'Δευτερεύον Email',
+	'Email' => 'Πρωτεύον Email',
 
 	//Added for Existing Picklist Entries
 
-	'--None--'=>'--None--',
-	'Mr.'=>'Mr.',
-	'Ms.'=>'Ms.',
-	'Mrs.'=>'Mrs.',
-	'Dr.'=>'Dr.',
-	'Prof.'=>'Prof.',
+	'--None--'=>'--Κενό--',
+	'Mr.'=>'Κ.',
+	'Ms.'=>'Δς.',
+	'Mrs.'=>'Κα.',
+	'Dr.'=>'Δρ.',
+	'Prof.'=>'Καθ.',
 
 	//Lead Status Picklist values
-	'Attempted to Contact'=>'Attempted to Contact',
-	'Cold'=>'Cold',
-	'Contact in Future'=>'Contact in Future',
-	'Contacted'=>'Contacted',
-	'Hot'=>'Hot',
-	'Junk Lead'=>'Junk Lead',
-	'Lost Lead'=>'Lost Lead',
-	'Not Contacted'=>'Not Contacted',
-	'Pre Qualified'=>'Pre Qualified',
-	'Qualified'=>'Qualified',
-	'Warm'=>'Warm',
+	'Attempted to Contact'=>'Προσπάθεια επικοινωνίας',
+	'Cold'=>'Κρύο',
+	'Contact in Future'=>'Επικοινωνία στο μέλλον',
+	'Contacted'=>'Έγινε επαφή',
+	'Hot'=>'Καυτό',
+	'Junk Lead'=>'Ανεπιθύμητη σύσταση',
+	'Lost Lead'=>'Χαμένη σύσταση',
+	'Not Contacted'=>'Δεν έγινε επαφή',
+	'Pre Qualified'=>'Προεπιλεγμένη',
+	'Qualified'=>'Πιστοποιημένη',
+	'Warm'=>'Ζεστή',
 
 	// Mass Action
-	'LBL_CONVERT_LEAD' => 'Convert Lead',
+	'LBL_CONVERT_LEAD' => 'Μετατροπή σύστασης',
 
 	//Convert Lead
-	'LBL_TRANSFER_RELATED_RECORD' => 'Transfer related record to',
-	'LBL_CONVERT_LEAD_ERROR' => 'You have to enable either Organization or Contact to convert the Lead',
-	'LBL_CONVERT_LEAD_ERROR_TITLE' => 'Modules Disabled',
-	'CANNOT_CONVERT' => 'Cannot Convert',
-	'LBL_FOLLOWING_ARE_POSSIBLE_REASONS' => 'Possible reasons include:',
-	'LBL_LEADS_FIELD_MAPPING_INCOMPLETE' => 'Leads Field Mapping is incomplete(Settings > Module Manager > Leads > Leads Field Mapping)',
-	'LBL_MANDATORY_FIELDS_ARE_EMPTY' => 'Mandatory fields are empty',
-	'LBL_LEADS_FIELD_MAPPING' => 'Leads Field Mapping',
+	'LBL_TRANSFER_RELATED_RECORD' => 'Μεταφορά σχετικών εγγραφών σε',
+	'LBL_CONVERT_LEAD_ERROR' => 'Πρέπει να ενεργοποιήσετε τους Οργανισμούς ή τις Επαφές για να μετατρέψετε τη σύσταση',
+	'LBL_CONVERT_LEAD_ERROR_TITLE' => 'Οι ενότητες δεν είναι ενεργές',
+	'CANNOT_CONVERT' => 'Δεν μπορεί να γίνει η μετατροπή',
+	'LBL_FOLLOWING_ARE_POSSIBLE_REASONS' => 'Πιθανοί λόγοι:',
+	'LBL_LEADS_FIELD_MAPPING_INCOMPLETE' => 'Η αντιστοίχιση των πεδίων είναι ημιτελής (Ρυθμίσεις > Διαχειριστής ενοτήτων > Συστάσεις > Αντιστοίχιση πεδίων συστάσεων)',
+	'LBL_MANDATORY_FIELDS_ARE_EMPTY' => 'Υποχρεωτικά πεδία είναι κενά',
+	'LBL_LEADS_FIELD_MAPPING' => 'Αντιστοίχιση πεδίων συστάσεων',
 
 	//Leads Custom Field Mapping
-	'LBL_CUSTOM_FIELD_MAPPING'=> 'Edit Field Mapping',
-	'LBL_WEBFORMS' => 'Setup Webforms',
+	'LBL_CUSTOM_FIELD_MAPPING'=> 'Τροποποίηση αντιστοίχισης πεδίων',
+	'LBL_WEBFORMS' => 'Εγκατάσταση δικτυακής φόρμας',
 );
 $jsLanguageStrings = array(
-	'JS_SELECT_CONTACTS' => 'Select Contacts to proceed',
-	'JS_SELECT_ORGANIZATION' => 'Select Organization to proceed',
-	'JS_SELECT_ORGANIZATION_OR_CONTACT_TO_CONVERT_LEAD' => 'Conversion requires selection of Contact or Organization'
+	'JS_SELECT_CONTACTS' => 'Επιλογή επαφών για συνέχιση',
+	'JS_SELECT_ORGANIZATION' => 'Επιλογή οργανισμών για συνέχιση',
+	'JS_SELECT_ORGANIZATION_OR_CONTACT_TO_CONVERT_LEAD' => 'Η μετατροπή απαιτεί επιλογή Επαφής ή Οργανισμού'
 );
