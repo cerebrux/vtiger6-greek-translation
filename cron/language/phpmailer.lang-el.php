9dcd7c0865aedc46f7f0164107918a446d007a76
Salih Emin <cerebrux.gr@gmail.com>
2014-03-15 00:21:46 +0200

    Μετάφραση κατα 100%

--- cron/language/phpmailer.lang-el.php
+++ cron/language/phpmailer.lang-el.php
@@ -15,18 +15,18 @@
  * @todo check if this file is used or not
  */
 $PHPMAILER_LANG = array();
-$PHPMAILER_LANG["provide_address"] = 'You must provide at least one ' .
-                                     'recipient email address.';
-$PHPMAILER_LANG["mailer_not_supported"] = ' mailer is not supported.';
-$PHPMAILER_LANG["execute"] = 'Could not execute: ';
-$PHPMAILER_LANG["instantiate"] = 'Could not instantiate mail function.';
-$PHPMAILER_LANG["authenticate"] = 'SMTP Error: Could not authenticate.';
-$PHPMAILER_LANG["from_failed"] = 'The following From address failed: ';
-$PHPMAILER_LANG["recipients_failed"] = 'SMTP Error: The following ' .
-                                       'recipients failed: ';
-$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Error: Data not accepted.';
-$PHPMAILER_LANG["connect_host"] = 'SMTP Error: Could not connect to SMTP host.';
-$PHPMAILER_LANG["file_access"] = 'Could not access file: ';
-$PHPMAILER_LANG["file_open"] = 'File Error: Could not open file: ';
-$PHPMAILER_LANG["encoding"] = 'Unknown encoding: ';
+$PHPMAILER_LANG["provide_address"] = 'Θα πρέπει να καθορίσετε τουλάχιστον έναν' .
+                                     'παραλήπτη με email.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' Η υπηρεσία mailer δεν υποστηρίζεται.';
+$PHPMAILER_LANG["execute"] = 'Αδυναμία εκτέλεσης: ';
+$PHPMAILER_LANG["instantiate"] = 'Αδυναμία δημιουργίας υπηρεσίας mai.';
+$PHPMAILER_LANG["authenticate"] = 'Σφάλμα SMTP: Αδυναμία πιστοποίησης.';
+$PHPMAILER_LANG["from_failed"] = 'Το παρακάτω Απο διέυθηνση απάτυχε: ';
+$PHPMAILER_LANG["recipients_failed"] = 'Σφάλμα SMTP: Ο παρακάτω ' .
+                                       'παραλήπτης απέτυχε: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'Σφάλμα SMTP: Δεδομένα μη αποδεκτά.';
+$PHPMAILER_LANG["connect_host"] = 'Σφάλμα SMTP: Αδυναμία σύνδεσης στον SMTP host.';
+$PHPMAILER_LANG["file_access"] = 'Αδυναμία πρόσβασης στο αρχείο: ';
+$PHPMAILER_LANG["file_open"] = 'Σφάλμα αρχείου: Αδυναμία ανοίγματος αρχείου: ';
+$PHPMAILER_LANG["encoding"] = 'Άγνωστη κωδικοποίηση χαρακτήρων: ';
 ?>
