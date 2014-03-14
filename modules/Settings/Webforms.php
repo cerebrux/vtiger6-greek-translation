06edb678981e0f9b0e59edc6e11cd142fe4d3b9b
Salih Emin <cerebrux.gr@gmail.com>
2014-03-15 01:49:09 +0200

    Revert to Δικτυακής Φόρμα

--- modules/Settings/Webforms.php
+++ modules/Settings/Webforms.php
@@ -8,36 +8,36 @@
  * All Rights Reserved.
  *************************************************************************************/
 $languageStrings = array(
-	'SINGLE_Settings:Webforms' => 'Webform',
+	'SINGLE_Settings:Webforms' => 'Δικτυακή Φόρμα',
 	//Basic Field Names
-	'WebForm Name' => 'Όνομα Webform',
+	'WebForm Name' => 'Όνομα Δικτυακής Φόρμα',
 	'Public Id' => 'Δημόσιο Id',
 	'Enabled' => 'Κατάσταση',
 	'Module' => 'Άρθρωμα',
 	'Return Url' => 'Url Επιστροφής',
 	'Post Url' => 'Url Προηγούμενο',
-	'SINGLE_Webforms' => 'Webform',
+	'SINGLE_Webforms' => 'Δικτυακή Φόρμα',
 
 	//Actions
 	'LBL_SHOW_FORM' => 'Προβολή Φόρμας',
-	'LBL_DUPLICATES_EXIST' => 'Το όνομα του Webform υπάρχει ήδη',
+	'LBL_DUPLICATES_EXIST' => 'Το όνομα της Δικτυακής Φόρμας υπάρχει ήδη',
 
 	//Blocks
-	'LBL_WEBFORM_INFORMATION' => 'Πληροφορίες Webform',
+	'LBL_WEBFORM_INFORMATION' => 'Πληροφορίες Δικτυακής Φόρμας',
 	'LBL_FIELD_INFORMATION' => 'Πληροφορίες Πεδίου',
 	'LBL_FIELD_NAME' => 'Όνομα Πεδίου',
 	'LBL_OVERRIDE_VALUE' => 'Αντικατάσταση Τιμής',
 	'LBL_MANDATORY' => 'Υποχρεωτικό',
-	'LBL_WEBFORM_REFERENCE_FIELD' => 'Webforms πεδίου αναφοράς',
+	'LBL_WEBFORM_REFERENCE_FIELD' => 'Πεδίο αναφοράς Δικτυακής Φόρμας',
 	'LBL_SELECT_FIELDS_OF_TARGET_MODULE' => 'Επιλέξτε Πεδία για το Άρθρομα προορισμού...',
-	'LBL_ALLOWS_YOU_TO_MANAGE_WEBFORMS' => 'Σας επιτρέπει να διαχειρίζεσται τα webforms',
+	'LBL_ALLOWS_YOU_TO_MANAGE_WEBFORMS' => 'Σας επιτρέπει να διαχειρίζεστε τις Δικτυακές Φόρμες',
 	'LBL_ADD_FIELDS' => 'Προσθήκη πεδίων',
 	'LBL_EMBED_THE_FOLLOWING_FORM_IN_YOUR_WEBSITE' => 'Ενσωματώστε την παρακάτω φόρμα στην ιστοσελίδα σας',
 	'LBL_SELECT_VALUE' => 'Επιλέξτε τιμή',
 	'LBL_LABEL' => 'ετικέτα',
 );
 $jsLanguageStrings = array(
-	'JS_WEBFORM_DELETED_SUCCESSFULLY' => 'Το Webform διεγράφτικε επιτυχώς',
+	'JS_WEBFORM_DELETED_SUCCESSFULLY' => 'Η Δικτυακή Φόρμα διαγράφτικε επιτυχώς',
 	'JS_LOADING_TARGET_MODULE_FIELDS' => 'Φόρτωση Πεδίων του Αρθρώματος',
 	'JS_SELECT_AN_OPTION' => 'Επιλέξτε μια επιλογή',
 	'JS_LABEL' => 'ετικέτα',
