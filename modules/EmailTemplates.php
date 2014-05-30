fa243b2f9138cf47214105353adf2fbff9a26506
Salih Emin <cerebrux.gr@gmail.com>
2014-05-30 13:10:48 +0300

    διορθωση typo

--- modules/EmailTemplates.php
+++ modules/EmailTemplates.php
@@ -22,12 +22,12 @@ $languageStrings = array(
 	'LBL_EMAIL_TEMPLATES'=> 'Πρότυπα Email',
 	'LBL_EMAIL_TEMPLATE' => 'Πρότυπο Email',
 	
-	'LBL_TEMPLATE_NAME' => 'Όνομα Προύπου',
+	'LBL_TEMPLATE_NAME' => 'Όνομα Προτύπου',
 	'LBL_DESCRIPTION' => 'Περιγραφή',
 	'LBL_SUBJECT' => 'Θέμα',
 	'LBL_GENERAL_FIELDS' => 'Γενικά Πεδία',
 	'LBL_SELECT_FIELD_TYPE' => 'Επιλογή Τύπου Πεδίου',
 	
-	'LBL_EMAIL_TEMPLATE_DESCRIPTION'=>'Διαχείρηση Προτύπων για το Άρθρωμα του Email',
+	'LBL_EMAIL_TEMPLATE_DESCRIPTION'=>'Διαχείριση Προτύπων για το Άρθρωμα του Email',
 	
 );
