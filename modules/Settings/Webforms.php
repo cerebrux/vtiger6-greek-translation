073da7b4b58baea2ea89087ef23cc294d61bb43b
Salih Emin <cerebrux.gr@gmail.com>
2014-10-23 13:35:19 +0300

    new 610 strings

--- modules/Settings/Webforms.php
+++ modules/Settings/Webforms.php
@@ -17,37 +17,44 @@
  *  -- More: https://github.com/cerebrux/vtiger6-greek-translation/graphs/contributors
  *************************************************************************************/
 $languageStrings = array(
-	'SINGLE_Settings:Webforms' => 'Δικτυακή Φόρμα',
-	//Basic Field Names
-	'WebForm Name' => 'Όνομα Δικτυακής Φόρμα',
-	'Public Id' => 'Δημόσιο Id',
+	'Captcha Enabled' => 'Captcha Enabled',
 	'Enabled' => 'Ενεργό',
-	'Module' => 'Άρθρωμα',
-	'Return Url' => 'Url Επιστροφής',
-	'Post Url' => 'Url Προηγούμενο',
-	'SINGLE_Webforms' => 'Δικτυακή Φόρμα',
-
-	//Actions
-	'LBL_SHOW_FORM' => 'Προβολή Φόρμας',
 	'LBL_DUPLICATES_EXIST' => 'Το όνομα της Δικτυακής Φόρμας υπάρχει ήδη',
-
-	//Blocks
 	'LBL_WEBFORM_INFORMATION' => 'Πληροφορίες Δικτυακής Φόρμας',
 	'LBL_FIELD_INFORMATION' => 'Πληροφορίες Πεδίου',
 	'LBL_FIELD_NAME' => 'Όνομα Πεδίου',
 	'LBL_OVERRIDE_VALUE' => 'Αντικατάσταση Τιμής',
+	'LBL_ROUNDROBIN_USERS_LIST' => 'Round Robin Users List',
+	'LBL_ENABLE_TARGET_MODULES_FOR_WEBFORM' => 'Enable Target modules for webform',	
+	'LBL_SAVE_FIELDS_ORDER' => 'Save fields order',
 	'LBL_MANDATORY' => 'Υποχρεωτικό',
+	'LBL_HIDDEN' => 'Hidden',
 	'LBL_WEBFORM_REFERENCE_FIELD' => 'Πεδίο αναφοράς Δικτυακής Φόρμας',
 	'LBL_SELECT_FIELDS_OF_TARGET_MODULE' => 'Επιλέξτε Πεδία για το Άρθρωμα προορισμού...',
 	'LBL_ALLOWS_YOU_TO_MANAGE_WEBFORMS' => 'Σας επιτρέπει να διαχειρίζεστε τις Δικτυακές Φόρμες',
+	'LBL_ASSIGN_USERS' => 'User Assignment',	
+	'LBL_ASSIGN_ROUND_ROBIN' => 'Assign Users In Round Robin',
 	'LBL_ADD_FIELDS' => 'Προσθήκη πεδίων',
 	'LBL_EMBED_THE_FOLLOWING_FORM_IN_YOUR_WEBSITE' => 'Ενσωματώστε την παρακάτω φόρμα στην ιστοσελίδα σας',
+	'LBL_SHOW_FORM' => 'Προβολή Φόρμας',	
 	'LBL_SELECT_VALUE' => 'Επιλέξτε τιμή',
 	'LBL_LABEL' => 'ετικέτα',
+	'Module' => 'Άρθρωμα',
+	'Post Url' => 'Url Προηγούμενο',
+	'Public Id' => 'Δημόσιο Id',
+	'Return Url' => 'Url Επιστροφής',
+	'SINGLE_Settings:Webforms' => 'Δικτυακή Φόρμα',
+	'SINGLE_Webforms' => 'Δικτυακή Φόρμα',
+	'WebForm Name' => 'Όνομα Δικτυακής Φόρμα',
 );
+
 $jsLanguageStrings = array(
-	'JS_WEBFORM_DELETED_SUCCESSFULLY' => 'Η Δικτυακή Φόρμα διαγράφτηκε επιτυχώς',
-	'JS_LOADING_TARGET_MODULE_FIELDS' => 'Φόρτωση Πεδίων του Αρθρώματος',
-	'JS_SELECT_AN_OPTION' => 'Επιλέξτε μια επιλογή',
 	'JS_LABEL' => 'ετικέτα',
+	'JS_LOADING_TARGET_MODULE_FIELDS' => 'Φόρτωση Πεδίων του Αρθρώματος',
+	'JS_MANDATORY_FIELDS_WITHOUT_OVERRIDE_VALUE_CANT_BE_HIDDEN' => 'Mandatory Fields without override values cant be hidden',
+	'JS_REFERENCE_FIELDS_CANT_BE_MANDATORY_WITHOUT_OVERRIDE_VALUE' => 'Reference fields cant be mandatory without override value',
+	'JS_SELECT_AN_OPTION' => 'Επιλέξτε μια επιλογή',
+	'JS_TYPE_TO_SEARCH' => 'Type to search',
+	'JS_WEBFORM_DELETED_SUCCESSFULLY' => 'Η Δικτυακή Φόρμα διαγράφτηκε επιτυχώς',
+	"JS_WEBFORM_WITH_THIS_NAME_ALREADY_EXISTS" => 'Webform with this name already exists',
 );
