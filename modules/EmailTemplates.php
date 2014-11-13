09546742cebb95aed86fe03129a28b3bfa80b7fd
Salih Emin <cerebrux.gr@gmail.com>
2014-11-13 13:40:53 +0200

    re-sync with upstream

--- modules/EmailTemplates.php
+++ modules/EmailTemplates.php
@@ -18,13 +18,13 @@
  *************************************************************************************/
 $languageStrings = array(
 	'LBL_ADD_RECORD' => 'Προσθέστε Πρότυπο Email',
-	'SINGLE_EmailTemplates' => 'Πρότυπο Email',
-	'LBL_EMAIL_TEMPLATES'=> 'Πρότυπα Email',
-	'LBL_EMAIL_TEMPLATE' => 'Πρότυπο Email',
-	'LBL_TEMPLATE_NAME' => 'Όνομα Προτύπου',
 	'LBL_DESCRIPTION' => 'Περιγραφή',
-	'LBL_SUBJECT' => 'Θέμα',
+	'LBL_EMAIL_TEMPLATE' => 'Πρότυπο Email',
+	'LBL_EMAIL_TEMPLATE_DESCRIPTION'=>'Διαχείριση Προτύπων για το Άρθρωμα του Email',
+	'LBL_EMAIL_TEMPLATES'=> 'Πρότυπα Email',
 	'LBL_GENERAL_FIELDS' => 'Γενικά Πεδία',
 	'LBL_SELECT_FIELD_TYPE' => 'Επιλογή Τύπου Πεδίου',
-	'LBL_EMAIL_TEMPLATE_DESCRIPTION'=>'Διαχείριση Προτύπων για το Άρθρωμα του Email',
+	'LBL_SUBJECT' => 'Θέμα',
+	'LBL_TEMPLATE_NAME' => 'Όνομα Προτύπου',
+	'SINGLE_EmailTemplates' => 'Πρότυπο Email',
 );
